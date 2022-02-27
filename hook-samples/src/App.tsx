c05a1705873aabd564a057d9279fcbe833191758
mzumi <masazumi.m@gmail.com>
2022-02-27 15:25:28 +0900

    Add PhraseInput

--- hook-samples/src/App.tsx
+++ hook-samples/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Checkbox } from './Checkbox'
+import { PhraseInput } from './PhraseInput'
 
 export const App: React.FC = () => {
   return (
-    <Checkbox />
+    <>
+      <Checkbox />
+      <br />
+      <PhraseInput />
+    </>
   )
 }
 
---
+++ hook-samples/src/PhraseInput.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+
+export const PhraseInput: React.FC<{}> = () => {
+  const [val, set] = useState("")
+  const [phrase, setPhrase] = useState("example phrase")
+
+  const createPhrase = () => {
+    setPhrase(val)
+    set("")
+  }
+
+  useEffect(() => {
+    console.log(`typing: "${val}"`)
+  }, [val])
+
+  useEffect(() => {
+    console.log(`saved phrase: "${phrase}"`)
+  }, [phrase])
+
+  useEffect(() => {
+    console.log("either val or phrase has changed")
+  }, [val, phrase])
+
+  useEffect(() => {
+    console.log("only once after initial render")
+  }, [])
+
+  return (
+    <>
+      <label>Favorite phrase:</label>
+      <input
+        value={val}
+        placeholder={phrase}
+        onChange={e => set(e.target.value)}
+      />
+      <button onClick={createPhrase}>send</button>
+    </>
+  )
+}
